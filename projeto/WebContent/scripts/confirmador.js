71607126ac78ef12e6f78e1359108aa3db803405
maari95 <maarinagomes1@gmail.com>
2023-05-01 23:37:17 -0300

    Update confirmador.js

--- projeto/WebContent/scripts/confirmador.js
+++ projeto/WebContent/scripts/confirmador.js
@@ -2,9 +2,18 @@
  * 
  */
 function confirmar(id){
-	let resposta = confirm("Confirmar a exclusão desse produto ?")
+	let resposta = confirm("Confirmar a exclusão ?")
 	if (resposta === true){
 		//alert(id)
 		window.location.href = "delete?id=" + id
 	}
 }
+
+function confirmarB(idBebidas){
+	let resposta = confirm("Confirmar a exclusão ?")
+	if (resposta === true){
+		//alert(id)
+		window.location.href = "deleteBebidas?idBebidas=" + idBebidas
+	}
+}
+
