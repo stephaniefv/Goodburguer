96c0736d3c4481b1e5d292032cfe696fc610ed73
maari95 <maarinagomes1@gmail.com>
2023-05-01 23:37:11 -0300

    Update validador.js

--- projeto/WebContent/scripts/validador.js
+++ projeto/WebContent/scripts/validador.js
@@ -8,7 +8,7 @@ function validar(){
 	let valorUni = frmProduto.valorUni.value
 	let valorTotal = frmProduto.valorTotal.value
 	if(nome === ""){
-		alert('Informe o nome Nome do produto')
+		alert('Informe o nome do produto')
 		frmProduto.nome.focus()
 		return false
 	}else if (quantidade === ""){
@@ -25,5 +25,36 @@ function validar(){
 		return false
 	}else{
 		document.forms["frmProduto"].submit()
+	}
+}
+
+function validarBebidas(){
+	let nomeBebidas = frmBebidas.nomeBebidas.value
+	let tamanho = frmBebidas.tamanho.value
+	let quantidadeBebidas = frmBebidas.quantidadeBebidas.value
+	let valorUniBebidas = frmBebidas.valorUniBebidas.value
+	let valorTotalBebidas = frmBebidas.valorTotalBebidas.value
+	if(nomeBebidas === ""){
+		alert('Informe o nome da bebida')
+		frmBebidas.nomeBebidas.focus()
+		return false
+	}else if (tamanho === ""){
+		alert('Informe o tamanho da bebida')
+		frmBebidas.tamanho.focus()
+		return false
+	}else if (quantidadeBebidas === ""){
+		alert('Informe a Quantidade da bebida')
+		frmBebidas.quantidadeBebidas.focus()
+		return false
+	}else if (valorUniBebidas === ""){
+		alert('Informe o valor da unidade')
+		frmBebidas.valorUniBebidas.focus()
+		return false
+	}else if (valorTotalBebidas === ""){
+		alert('Informe o valor total')
+		frmBebidas.valorTotalBebidas.focus()
+		return false
+	}else{
+		document.forms["frmBebidas"].submit()
 	}
 }
